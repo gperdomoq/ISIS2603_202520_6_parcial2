a189ed350a2dc2e3cf61755880d62c65dcadb478
Gabriela <g.perdomoq@uniandes.edu.co>
2025-11-24 15:27:29 -0500

    entrega

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
+import { ArtiststService } from './artists/artistst.service';
+import { AlbumComponent } from './albums/album.component';
+
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [	AppComponent,
+      AlbumComponent
+   ],
   imports: [BrowserModule, AppRoutingModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
